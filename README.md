79bc9503d74075acf372a1f6613ffff6b690fbf3
Xiao Li <xiaoli3397@gmail.com>
2025-05-10 22:06:52 +0800

    update

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Remove arXiv HTML annoying banner and report button.
 
-![image](https://s2.loli.net/2025/05/10/1KXxZYUOnTLsE9M.png)
+![image](https://s2.loli.net/2025/05/10/ZWgi7rRcCwMAfPI.png)
 
 Open at Greasy Fork: [arXiv HTML cleaner](https://greasyfork.org/zh-CN/scripts/535535-arxiv-html-cleaner)
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         arXiv HTML cleaner
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/undefined443
 // @version      0.0.1
 // @description  Remove arXiv HTML annoying banner and report button.
 // @author       undefined443
@@ -10,28 +10,29 @@
 // @license      MIT
 // ==/UserScript==
 
-(function() {
-  'use strict';
+(function () {
+  "use strict";
   function removeElement(query) {
-      const element = document.querySelector(query);
-      if (element) {
-          element.remove();
-      }
+    const element = document.querySelector(query);
+    if (element) {
+      element.remove();
+    }
   }
 
   const observer = new MutationObserver((mutationsList) => {
-      for (const mutation of mutationsList) {
-          if (mutation.type === 'childList') {
-              removeElement('.desktop_header');
-              removeElement('#openForm');
-              removeElement('#small-report-button');
-          }
+    for (const mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        removeElement(".desktop_header");
+        removeElement(".package-alerts");
+        removeElement("#openForm");
+        removeElement("#small-report-button");
       }
+    }
   });
-
 
   const config = { childList: true, subtree: true };
   observer.observe(document.body, config);
-  removeElement('.desktop_header');
-  removeElement('#openForm');
+  removeElement(".desktop_header");
+  removeElement(".package-alerts");
+  removeElement("#openForm");
 })();
