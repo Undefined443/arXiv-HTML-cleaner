00d59cf2cad808a37bc113290a7c9251b0535692
Xiao Li <xiaoli3397@gmail.com>
2025-07-24 18:47:34 +0800

    Remove LaTeX error message

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         arXiv HTML cleaner
 // @namespace    https://github.com/undefined443
-// @version      0.0.3
+// @version      0.0.4
 // @description  Remove arXiv HTML annoying banner and report button.
 // @author       undefined443
 // @match        https://arxiv.org/html/*
 // @match        https://arxiv.org/abs/*
+// @match        https://ar5iv.labs.arxiv.org/html/*
 // @icon         https://static.arxiv.org/static/browse/0.3.4/images/icons/favicon-32x32.png
 // @grant        none
 // @license      MIT
+// @downloadURL https://update.greasyfork.org/scripts/535535/arXiv%20HTML%20cleaner.user.js
+// @updateURL https://update.greasyfork.org/scripts/535535/arXiv%20HTML%20cleaner.meta.js
 // ==/UserScript==
 
 (function () {
@@ -26,6 +29,7 @@
       if (mutation.type === "childList") {
         removeElement(".desktop_header");
         removeElement(".package-alerts");
+        removeElement(".ltx_ERROR");
         removeElement("#openForm");
         removeElement("#small-report-button");
       }
@@ -36,9 +40,10 @@
   observer.observe(document.body, config);
 
   // Static removal of elements
-  removeElement(".desktop_header");  // Remove top banner at html page
-  removeElement(".package-alerts");  // Remove package alerts at html page
-  removeElement(".slider-wrapper");  // Remove top banner at abs page
-  removeElement("#openForm");
-  removeElement("#cu-identity");     // Remove Cornell University identity at abs page
+  removeElement(".desktop_header"); // Remove top banner at html page
+  removeElement(".package-alerts"); // Remove package alerts at html page
+  removeElement(".slider-wrapper"); // Remove top banner at abs page
+  removeElement(".ltx_ERROR");      // Remove LaTeX error message
+  removeElement("#openForm");       // Remove open form button at html page
+  removeElement("#cu-identity");    // Remove Cornell University identity at abs page
 })();
